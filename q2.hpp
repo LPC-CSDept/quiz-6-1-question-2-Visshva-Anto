4d04c8e187ec302f01c1a9e99cea4a92d8f6a449
Visshva-Anto <123528068+Visshva-Anto@users.noreply.github.com>
2023-03-24 01:00:24 +0000

    1

--- q2.hpp
+++ q2.hpp
@@ -5,3 +5,29 @@ using namespace std;
 // ******************************
 // Implement all your functions here
 // ******************************
+void getinput(int &, int&, int &);
+int findMin(int, int, int);
+void printResult(int, int, int, int);
+
+void getinput(int &n1, int&n2, int &n3)
+{
+    cout << "Enter 3 number: \n";
+    cin >> n1 >> n2 >> n3;
+}
+int findMin(int n1, int n2, int n3)
+{
+    int min;
+    if((n1<n2) && (n1<n3))
+        min=n1;
+
+    else if((n2<n1) && (n2<n3))
+        min=n2;
+
+    else
+        min=n3;
+    return min;
+}
+void printResult(int n1, int n2, int n3, int min)
+{
+    cout << "The minimum is: \n" << min << endl;
+}
